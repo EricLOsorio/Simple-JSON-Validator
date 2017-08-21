847a54fb390542492cc56eb15cdb98e6adb55aa9
EricLOSorio <erioso@verizon.net>
2017-08-21 16:46:52 -0400

    fixed pattern to give more accurate json validations

--- jason_validator.js
+++ jason_validator.js
@@ -25,11 +25,10 @@ function purgeSpaces(string){
 
   button.addEventListener('click',function(){
   	var originalString=inputString.value;
-    var testString=purgeSpaces(originalString);
-    var pattern=/^\{(?:\"(?:[a-zA-Z_]+_*\d*[a-zA-Z]*)+\"\:(?:(?:(?:(?:true)?|(?:false)?)|\d*)|(?:\")(?:[^\"])+(?:\"))){1}(?:,\"(?:[a-zA-Z_]+_*\d*[a-zA-Z]*)+\"\:(?:(?:(?:(?:true)?|(?:false)?)|\d*)|(?:\")(?:[^\"\{\}])+(?:\")))*\}$/;
+    var testString=purgeSpaces(originalString); console.log(testString);
+    var pattern=/^\{(((\"([a-zA-Z_]+_*\d*[a-zA-Z]*)+\"\:)+)((\d*)|(true|false)|((\"([^\"]*)\"))|((\[((\"[^\"]*\")|\d*|(true|false)))(((,\"[^\"]*\")|(,\d+)|,(true|false)))*\])|(\{\}))?((,(\"([a-zA-Z_]+_*\d*[a-zA-Z]*)+\"\:){1}((\d+)|(true|false)+|((\"([^\"]*)\"))+|((\[((\"[^\"]*\")|\d*|(true|false)))(((,\"[^\"]*\")|(,\d+)|,(true|false)))*\])|(\{\}))+)*)?)?\}/g;
     if(pattern.test(testString)){
-      output.innerHTML="The input JSON string is valid JSON \r\r\r"+
-      pattern.exec(testString);
+      output.innerHTML=`The input JSON string is valid JSON \r\r\r ${testString}`
     } else{
       output.innerHTML="The input JSON string is invalid. "+
       " Properties and values must be surrounded by quotes. The colon (:) must separate each property from its value. The string must start and end with curly brackets. Valid values are either, boolean(true or false), number, or string. A comma must be placed at the end of each property/value pair except for the LAST property/value pair in the string."
